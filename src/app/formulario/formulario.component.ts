c413b0c10d0bda543bcbd41a46d1d92189780761
Jorgep503 <jorge.p.391@gmail.com>
2022-03-23 22:14:16 -0600

    Cambio de Two-Binding a Referenicas Locales

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -10,11 +10,11 @@ export class FormularioComponent {
 
   @Output() personaCreada = new EventEmitter<Persona>();
 
-  nombreInput:string = '';
-  apellidoInput:string = '';
+  //nombreInput:string = '';
+  //apellidoInput:string = '';
 
-  agregarPersona(){
-    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+  agregarPersona(nombreInput:HTMLInputElement,apellidoInput:HTMLInputElement){
+    let persona1 = new Persona(nombreInput.value,apellidoInput.value);
     this.personaCreada.emit(persona1);
   }
 
