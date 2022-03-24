9f03009bc87c15b7845f4e04975a36477723933f
Jorgep503 <jorge.p.391@gmail.com>
2022-03-23 22:20:43 -0600

    ViewChild

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Directiva *ngFor
 Property Binding entre componentes en Angular
 Event Binding entre componentes en Angular
 Referencias Locales
+ViewChild
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model';
 
 @Component({
@@ -12,9 +12,13 @@ export class FormularioComponent {
 
   //nombreInput:string = '';
   //apellidoInput:string = '';
+  @ViewChild('nombreInput') nombreInput:ElementRef;
+  @ViewChild('apellidoInput') apellidoInput:ElementRef;
 
-  agregarPersona(nombreInput:HTMLInputElement,apellidoInput:HTMLInputElement){
-    let persona1 = new Persona(nombreInput.value,apellidoInput.value);
+  agregarPersona(){
+    let persona1 = new Persona(
+      this.nombreInput.nativeElement.value
+      ,this.apellidoInput.nativeElement.value);
     this.personaCreada.emit(persona1);
   }
 
