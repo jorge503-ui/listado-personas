cdfe181f196968fa1ee3b82a98b7a1b1e18c1e19
Jorgep503 <jorge.p.391@gmail.com>
2022-03-23 22:50:19 -0600

    Inyeccion de un servicio a otro servicio

--- src/app/persona.services.ts
+++ src/app/persona.services.ts
@@ -1,5 +1,8 @@
+import { Injectable } from "@angular/core";
+import { LoggingServices } from "./LoggingServices.services";
 import { Persona } from "./persona.model";
 
+@Injectable()
 export class PersonasServices{
     personas: Persona[]=[
         new Persona('Juan','Perez')
@@ -7,7 +10,10 @@ export class PersonasServices{
       , new Persona('Karla','Lara')
     ];
 
+    constructor(private loggingServices:LoggingServices){}
+
     agregarPersona(persona: Persona){
+      this.loggingServices.enviaMensajeAConsola("Agregamos a persona: "+persona.nombre)
         this.personas.push(persona);
       }
 }
